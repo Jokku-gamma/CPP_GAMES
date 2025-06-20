b87ed7b59fcbf8c5ab02581480b9193a5298fd57
Jokku-gamma <joelkuaba2004@gmail.com>
2025-06-20 12:10:41 +0530

    Modified main.cpp

--- SampleTest/main.cpp
+++ SampleTest/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <SDL2/SDL.h> 
-
+#include <cmath>  // For sin, cos
+#define SDL_MAIN_HANDLED
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 void logSDLError(std::ostream &os, const std::string &msg) {
     os << msg << " error: " << SDL_GetError() << std::endl;
 }
